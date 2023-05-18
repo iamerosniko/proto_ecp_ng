41f68fd343e90a6f60ff399396b3016fd3b12736
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2023-05-19 02:50:01 +0800

    test

--- ui/src/app/reports/reports.component.ts
+++ ui/src/app/reports/reports.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reports',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent {
+  form: FormGroup;
+  years: number[] = [];
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildForm();
+    this.populateYears();
+  }
+
+  buildForm() {
+    this.form = this.formBuilder.group({
+      team: ['', Validators.required],
+      year: ['', Validators.required]
+    });
+  }
+
+  populateYears() {
+    const currentYear = new Date().getFullYear();
+    const startYear = 2019;
+
+    for (let year = startYear; year <= currentYear; year++) {
+      this.years.push(year);
+    }
+  }
+
+  onSubmit() {
+    // Handle form submission here
+    if (this.form.valid) {
+      console.table(this.form.value)
+      console.log(this.form.value)
+    }
+  }
 }
